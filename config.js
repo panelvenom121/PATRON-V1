ea32f4fd9a2ee1b58636d6427473be89a3c1f0e5
panelvenom121 <panelvenom0@gmail.com>
2024-08-31 11:44:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@ global.location = '1'; // Replace with your location panel
 //===========================//
 
 global.prefa = ['', '!', '.', ',', '🐤', '🗿'];
-global.owner = ['2348133729715'];
-global.ownMain = '2348133729715';
+global.owner = ['2347086385139'];
+global.ownMain = '2347086385139';
 global.NamaOwner = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change 
 global.sessionName = 'session';
 global.connect = true; // Set to false if using QR Code
 global.namabot = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change
 global.author = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change
-global.packname = 'PATRON 🚹'; // Change if needed
+global.packname = '~✮͢≛⃝MR.🅳 ࿆͆᷍🅼demy🚬'; // Change if needed
 global.url = 'https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q'; // Do not change
 
 //===========================//
